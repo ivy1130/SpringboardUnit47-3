08f988c93ef9733aa31a4707a566896c85961316
Ivy <ivy@onprem.com>
2022-06-17 17:44:09 +0200

    Completed assignment.

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,22 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+
+  while (leftIdx <= rightIdx) {
+    // find the middle value
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    let leftVal = arr[middleIdx-1]
+
+    if (middleVal > 0) {
+      leftIdx = middleIdx + 1;
+    } else if (middleVal === 0 && leftVal < 1) {
+      rightIdx = middleIdx - 1;
+    } else {
+      return arr.length-middleIdx;
+    }
+  }
+  return 0
 }
 
 module.exports = countZeroes
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,46 @@
-function sortedFrequency() {
-
+function sortedFrequency(arr, num) {
+  let firstIdx = findFirst(arr, num);
+  if (firstIdx === -1) return firstIdx;
+  let lastIdx = findLast(arr, num);
+  return lastIdx - firstIdx + 1;
 }
+
+function findFirst(arr, num, leftIdx = 0, rightIdx = arr.length - 1) {
+  while (leftIdx <= rightIdx) {
+    // find the middle value
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    let leftVal = arr[middleIdx-1]
+
+    if (middleVal === num && (leftVal < num || middleIdx === 0)){
+      return middleIdx;
+    } else if (middleVal < num) {
+      leftIdx = middleIdx + 1;
+    } else {
+      rightIdx = middleIdx - 1;
+    }
+  }
+  return -1
+}
+
+function findLast(arr, num, leftIdx = 0, rightIdx = arr.length - 1) {
+  while (leftIdx <= rightIdx) {
+    // find the middle value
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    let rightVal = arr[middleIdx+1]
+
+    if (middleVal === num && (rightVal > num || middleIdx === arr.length-1)){
+      return middleIdx;
+    } else if (middleVal > num) {
+      rightIdx = middleIdx - 1;
+    }
+    else {
+      leftIdx = middleIdx + 1;
+    } 
+  }
+  return -1
+}
+
 
 module.exports = sortedFrequency
